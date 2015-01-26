85eb83070fe4bf32e8fb7ea2762676300348733e
prakhar_sahay <prakhar@brandeis.edu>
2015-01-26 09:49:14 -0500

    File pathways 8

--- dropdown.js
+++ dropdown.js
@@ -1,14 +1,14 @@
 	var x='<div id="blue_strip">'+
-	'<img id="blue_img" src="res/blue_text.png" width="150" onclick="location.href=\'html/index.php\'">'+
-	'<span id="blue_text" onclick="location.href=\'html/index.php\'">Student Union</span>'+
+	'<img id="blue_img" src="res/blue_text.png" width="150" onclick="location.href=\'html/index.html\'">'+
+	'<span id="blue_text" onclick="location.href=\'html/index.html\'">Student Union</span>'+
 	'<div id="menu_strip">'+
-	'	<div class="menu_tab" data-num="0" onclick="location.href=\'html/meet_the_team/index.php\'">MEET THE TEAM</div>'+
-	'	<div class="menu_tab" data-num="1" onclick="location.href=\'html/club_support/index.php\'">CLUB SUPPORT</div>'+
-	'	<div class="menu_tab" data-num="2" onclick="location.href=\'html/senate/index.php\'">SENATE</div>'+
-	'	<div class="menu_tab" data-num="3" onclick="location.href=\'html/transportation/index.php\'">TRANSPORTATION</div>'+
-	'	<!--<div class="menu_tab" data-num="4" onclick="location.href=\'html/resources/index.php\'">RESOURCES</div>'+
-	'	<div class="menu_tab" data-num="5" onclick="location.href=\'html/events/index.php\'">EVENTS</div>-->'+
-	'	<!--<div class="green_tab" onclick="location.href=\'committees.html\'">COMMITTEES</div>-->'+
+	'	<div class="menu_tab" data-num="0" onclick="location.href=\'html/meet_the_team/index.html\'">MEET THE TEAM</div>'+
+	'	<div class="menu_tab" data-num="1" onclick="location.href=\'html/club_support/index.html\'">CLUB SUPPORT</div>'+
+	'	<div class="menu_tab" data-num="2" onclick="location.href=\'html/senate/html\'">SENATE</div>'+
+	'	<div class="menu_tab" data-num="3" onclick="location.href=\'html/transportation/index.html\'">TRANSPORTATION</div>'+
+	'	<!--<div class="menu_tab" data-num="4" onclick="location.href=\'html/resources/index.html\'">RESOURCES</div>'+
+	'	<div class="menu_tab" data-num="5" onclick="location.href=\'html/events/index.html\'">EVENTS</div>-->'+
+	'	<!--<div class="green_tab" onclick="location.href=\'html/committees/index.html\'">COMMITTEES</div>-->'+
 	'</div>'+
 	'<div class="horiz_margin" style="position:absolute;top:80px;width:1020px"></div>'+
 '</div>';
