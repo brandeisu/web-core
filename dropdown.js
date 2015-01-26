dac494e33c0cb15cf9c9cb9c52c4b41edbac8e28
prakhar_sahay <prakhar@brandeis.edu>
2015-01-26 09:47:13 -0500

    File pathways 7

--- dropdown.js
+++ dropdown.js
@@ -1,3 +1,20 @@
+	var x='<div id="blue_strip">'+
+	'<img id="blue_img" src="res/blue_text.png" width="150" onclick="location.href=\'html/index.php\'">'+
+	'<span id="blue_text" onclick="location.href=\'html/index.php\'">Student Union</span>'+
+	'<div id="menu_strip">'+
+	'	<div class="menu_tab" data-num="0" onclick="location.href=\'html/meet_the_team/index.php\'">MEET THE TEAM</div>'+
+	'	<div class="menu_tab" data-num="1" onclick="location.href=\'html/club_support/index.php\'">CLUB SUPPORT</div>'+
+	'	<div class="menu_tab" data-num="2" onclick="location.href=\'html/senate/index.php\'">SENATE</div>'+
+	'	<div class="menu_tab" data-num="3" onclick="location.href=\'html/transportation/index.php\'">TRANSPORTATION</div>'+
+	'	<!--<div class="menu_tab" data-num="4" onclick="location.href=\'html/resources/index.php\'">RESOURCES</div>'+
+	'	<div class="menu_tab" data-num="5" onclick="location.href=\'html/events/index.php\'">EVENTS</div>-->'+
+	'	<!--<div class="green_tab" onclick="location.href=\'committees.html\'">COMMITTEES</div>-->'+
+	'</div>'+
+	'<div class="horiz_margin" style="position:absolute;top:80px;width:1020px"></div>'+
+'</div>';
+document.getElementsByTagName("body")[0].innerHTML=x;
+
+
 var menu_strip=document.getElementById("menu_strip");
 var tabs=menu_strip.getElementsByClassName("menu_tab");
 var dropdowns=menu_strip.getElementsByClassName("green_dropdown");
